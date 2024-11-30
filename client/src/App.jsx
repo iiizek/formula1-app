3a30c585abfdb6dc14e023c75687f30ec86f6ca7
Anton Gudenko <antong702@gmail.com>
2024-11-30 10:17:09 +0300

    bebe s baba

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,55 @@
+import { useState, useEffect } from 'react';
+import { Routes, Route } from 'react-router-dom';
+
+import MainPage from './pages/MainPage';
+import TablePage from './pages/TablePage';
+import { Switch } from './components/ui/switch';
+import { Card } from './components/ui/card';
+
+import { FileSpreadsheetIcon } from 'lucide-react';
+import { Moon } from 'lucide-react';
+
 function App() {
-	return <></>;
+	const [isDarkTheme, setIsDarkTheme] = useState(() => {
+		const storedTheme = localStorage.getItem('theme');
+		if (storedTheme) {
+			return storedTheme === 'dark';
+		}
+		return window.matchMedia('(prefers-color-scheme: dark)').matches;
+	});
+
+	useEffect(() => {
+		if (isDarkTheme) {
+			document.documentElement.classList.add('dark');
+			localStorage.setItem('theme', 'dark');
+		} else {
+			document.documentElement.classList.remove('dark');
+			localStorage.setItem('theme', 'light');
+		}
+	}, [isDarkTheme]);
+
+	return (
+		<div className='flex flex-col gap-4 p-8'>
+			<Card className='flex justify-between items-center p-8'>
+				<h1 className='flex items-center gap-4 font-bold text-3xl'>
+					<FileSpreadsheetIcon size={32} className='text-primary' />
+					Formula 1 | Редактор табличных данных
+				</h1>
+
+				<Card className='flex items-center gap-2 p-2'>
+					<Switch
+						checked={isDarkTheme}
+						onClick={() => setIsDarkTheme(!isDarkTheme)}
+					/>
+					<Moon size={24} className='text-primary' />
+				</Card>
+			</Card>
+			<Routes>
+				<Route path='/' element={<MainPage />} />
+				<Route path='/tables/:tableId' element={<TablePage />} />
+			</Routes>
+		</div>
+	);
 }
 
 export default App;
